66e50c0218b889f3bcf70a27432245f7b4123ebf
ssrskl <1071352028@qq.com>
2020-04-03 09:47:04 +0800

    gittest

---
+++ gitTest/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author Maoyan
+ * @data 2020/4/3 9:38
+ * @Version 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
